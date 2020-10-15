f8dd5edda2e2c30d10ca6dc94576bf2c2fcfb142
Jefferraz <jeffersonferraz.05@hotmail.com>
2020-10-15 05:27:40 -0300

    First update

---
+++ Testando.py
@@ -0,0 +1,68 @@
+"""
+nome = input('Qual é o seu nome?')
+idade = input('Qual é a sua idade?')
+peso = input('Qual é o seu peso?')
+
+print(nome, idade, peso)
+"""
+
+"""
+nome = input("Qual é o seu nome?")
+print('Ola ' + nome + '! Prazer em te conhecer!')
+"""
+
+"""
+dia = input('Dia:')
+mes = input('Mes:')
+ano = input('Ano:')
+
+print('Você nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + '. Correto?')
+"""
+
+"""
+#Atribuição de tipo primitivo para realizar operações matamáticas;
+#Sintaxe do print; 
+
+soma1 = int(input('Digite o primeito nunmero:'))
+soma2 = int(input('Digite o segundo numero:'))
+
+soma = soma1 + soma2
+
+print('A soma entre {} e {} vale {}'.format(soma1, soma2, soma))
+"""
+
+"""
+#Consultando tipagem de variáveis;
+
+info = input('Digite uma informação:')
+print(type(info))
+print(info.isalnum())
+print(info.isalpha())
+print(info.isdecimal())
+print(info.islower())
+"""
+
+""""
+#Programa para calcular média;
+
+nome = input('Digite o nome do aluno:')
+
+n1 = float(input('Digite a nota do primeiro bimestre:'))
+
+n2 = float(input('Digite a nota do segundo bimestre:'))
+
+n3 = float(input('Digite a nota do terceiro bimestre:'))
+
+n4 = float(input('Digite a nota do quarto bimestre:'))
+
+media = (n1 + n2 + n3 + n4) / 4
+
+if media >= 6:
+    print('As notas obtidas pelo aluno:', nome,
+          'foram: {} # {} # {} # {}. E sua média é: {}'.format(n1, n2, n3, n4, media))
+    print('Parabéns, você foi aprovado!')
+else:
+    print('As notas obtidas pelo aluno:', nome,
+          'foram: {} # {} # {} # {}. E sua média é: {}'.format(n1, n2, n3, n4, media))
+    print('Você foi reprovado, estude mais!')
+"""
